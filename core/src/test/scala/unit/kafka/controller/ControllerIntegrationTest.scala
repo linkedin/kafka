2a91a2f033d055906142c4ff7f32285bc51698f3
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2020-12-16 18:04:59 -0800

    Delete commented out code I left behind.

--- core/src/test/scala/unit/kafka/controller/ControllerIntegrationTest.scala
+++ core/src/test/scala/unit/kafka/controller/ControllerIntegrationTest.scala
@@ -586,20 +586,6 @@ class ControllerIntegrationTest extends ZooKeeperTestHarness {
     assertEquals(expectedShutdownEntries, zkClient.getBrokerShutdownEntries)
 
     // Now ensure that after the controller moves, shutdown is still rejected.
-    /*testControllerMove(() => {
-      @volatile var notEnoughReplicasDetected = false
-      controller.controlledShutdown(1, servers.find(_.config.brokerId == 1).get.kafkaController.brokerEpoch, {
-        case scala.util.Failure(exception) if exception.isInstanceOf[NotEnoughReplicasException] => notEnoughReplicasDetected = true
-        case _ =>
-      })
-
-      TestUtils.waitUntilTrue(() => notEnoughReplicasDetected, "Fail to detect expected NotEnoughReplicasException")
-
-      val expectedShutdownEntries = Map(2 -> 49)
-      assertEquals(expectedShutdownEntries, zkClient.getBrokerShutdownEntries)
-    })*/
-
-    // Now ensure that after the controller moves, shutdown is still rejected.
     zkClient.deleteController(controller.controllerContext.epochZkVersion)
     TestUtils.waitUntilTrue(() => !controller.isActive, "Controller fails to resign")
 

59ef55f42634d1500e27690efff9b633950dbcb8
Lucas Wang <luwang@linkedin.com>
2021-04-24 10:11:40 -0700

    Using new config to control whether to use the async fetcher

--- core/src/main/scala/kafka/server/KafkaApis.scala
+++ core/src/main/scala/kafka/server/KafkaApis.scala
@@ -265,7 +265,11 @@ class KafkaApis(val requestChannel: RequestChannel,
         .setPartitionErrors(result.map { case (tp, error) => toStopReplicaPartition(tp, error) }.toBuffer.asJava)))
     }
 
-    CoreUtils.swallow(replicaManager.asyncReplicaFetcherManager.shutdownIdleFetcherThreads(), this)
+    if (config.liAsyncFetcherEnable) {
+      CoreUtils.swallow(replicaManager.asyncReplicaFetcherManager.shutdownIdleFetcherThreads(), this)
+    } else {
+      CoreUtils.swallow(replicaManager.replicaFetcherManager.shutdownIdleFetcherThreads(), this)
+    }
   }
 
   def handleUpdateMetadataRequest(request: RequestChannel.Request): Unit = {

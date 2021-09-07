07fa570b9a8254c55990513ae1ca1dfe2aa739b7
Lucas Wang <luwang@linkedin.com>
2021-09-07 14:32:47 -0700

    [LI-HOTFIX] Improving performance of the isReplicaOnline method
    TICKET = LIKAFKA-38536
    LI_DESCRIPTION =
    The ControllerContext.isReplicaOnline method is frequently called inside a loop,
    and internally this method relies on several derived fields in the ControllerContext.
    Repeatedly calculating the derived fields could be expensive, and yet these fields
    do not change between iterations of the loop.
    
    This PR creates a new class ControllerContextSnapshot that tries to cache
    the derived fields in order to save the repeated computation and speed up the
    controller.
    
    EXIT_CRITERIA = When this change is proposed in upstream kafka and pulled internally.

--- core/src/main/scala/kafka/controller/ControllerContext.scala
+++ core/src/main/scala/kafka/controller/ControllerContext.scala
@@ -225,14 +225,6 @@ class ControllerContext {
     }.toSet
   }
 
-  def isReplicaOnline(brokerId: Int, topicPartition: TopicPartition, includeShuttingDownBrokers: Boolean = false): Boolean = {
-    val brokerOnline = {
-      if (includeShuttingDownBrokers) liveOrShuttingDownBrokerIds.contains(brokerId)
-      else liveBrokerIds.contains(brokerId)
-    }
-    brokerOnline && !replicasOnOfflineDirs.getOrElse(brokerId, Set.empty).contains(topicPartition)
-  }
-
   def replicasOnBrokers(brokerIds: Set[Int]): Set[PartitionAndReplica] = {
     brokerIds.flatMap { brokerId =>
       partitionAssignments.flatMap {
@@ -257,8 +249,9 @@ class ControllerContext {
   }
 
   def allLiveReplicas(): Set[PartitionAndReplica] = {
+    val snapshot = ControllerContextSnapshot(this)
     replicasOnBrokers(liveBrokerIds).filter { partitionAndReplica =>
-      isReplicaOnline(partitionAndReplica.replica, partitionAndReplica.topicPartition)
+      snapshot.isReplicaOnline(partitionAndReplica.replica, partitionAndReplica.topicPartition)
     }
   }
 
@@ -270,12 +263,13 @@ class ControllerContext {
   def onlineAndOfflineReplicas: (Set[PartitionAndReplica], Set[PartitionAndReplica]) = {
     val onlineReplicas = mutable.Set.empty[PartitionAndReplica]
     val offlineReplicas = mutable.Set.empty[PartitionAndReplica]
+    val snapshot = ControllerContextSnapshot(this)
     for ((topic, partitionAssignments) <- partitionAssignments;
          (partitionId, assignment) <- partitionAssignments) {
       val partition = new TopicPartition(topic, partitionId)
       for (replica <- assignment.replicas) {
         val partitionAndReplica = PartitionAndReplica(partition, replica)
-        if (isReplicaOnline(replica, partition))
+        if (snapshot.isReplicaOnline(replica, partition))
           onlineReplicas.add(partitionAndReplica)
         else
           offlineReplicas.add(partitionAndReplica)
@@ -430,4 +424,23 @@ class ControllerContext {
   private def isValidPartitionStateTransition(partition: TopicPartition, targetState: PartitionState): Boolean =
     targetState.validPreviousStates.contains(partitionStates(partition))
 
+}
+
+/**
+  * The ControllerContextSnapshot is an immutable snapshot of the ControllorContext.
+  * The motivation for this class is that we don't need to calculate certain fields
+  * repeatedly, including liveBrokerIds and liveOrShuttingDownBrokerIds.
+  */
+case class ControllerContextSnapshot(controllerContext: ControllerContext) {
+  val liveBrokerIds = controllerContext.liveBrokerIds
+  val liveOrShuttingDownBrokerIds = controllerContext.liveOrShuttingDownBrokerIds
+  val replicasOnOfflineDirs = controllerContext.replicasOnOfflineDirs
+
+  def isReplicaOnline(brokerId: Int, topicPartition: TopicPartition, includeShuttingDownBrokers: Boolean = false): Boolean = {
+    val brokerOnline = {
+      if (includeShuttingDownBrokers) liveOrShuttingDownBrokerIds.contains(brokerId)
+      else liveBrokerIds.contains(brokerId)
+    }
+    brokerOnline && !replicasOnOfflineDirs.getOrElse(brokerId, Set.empty).contains(topicPartition)
+  }
 }

efbb73a6a10a89ba9f2f16579dcb0edcb5859303
Lucas Wang <luwang@linkedin.com>
2022-03-29 10:59:42 -0700

    Testing Java 11

---
+++ ByteBufferTest.java
@@ -0,0 +1,10 @@
+
+import java.nio.ByteBuffer;
+
+public class ByteBufferTest {
+  public static void main(String... args) {
+    ByteBuffer buffer = ByteBuffer.wrap(new byte[] { 1, 2, 3 });
+    buffer.position(1); 
+    System.out.println(buffer.get());
+  }
+}

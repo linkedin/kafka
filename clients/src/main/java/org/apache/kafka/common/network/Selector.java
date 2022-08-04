21d7c6b37333ba41538b822f421df70ab12438fc
Lucas Wang <luwang@linkedin.com>
2022-08-04 13:30:01 -0700

    Polishing the logging

--- clients/src/main/java/org/apache/kafka/common/network/Selector.java
+++ clients/src/main/java/org/apache/kafka/common/network/Selector.java
@@ -466,7 +466,7 @@ public class Selector implements Selectable, AutoCloseable {
         long startSelect = time.nanoseconds();
         int numReadyKeys = select(timeout);
         long endSelect = time.nanoseconds();
-        log.trace("Completed select in " + (endSelect - startSelect) + "ms");
+        log.trace("Completed select in " + (endSelect - startSelect) + "ns");
         this.sensors.selectTime.record(endSelect - startSelect, time.milliseconds());
 
 
@@ -647,8 +647,8 @@ public class Selector implements Selectable, AutoCloseable {
     private void attemptRead(SelectionKey key, KafkaChannel channel) throws IOException {
         //if channel is ready and has bytes to read from socket or buffer, and has no
         //previous receive(s) already staged or otherwise in progress then read from it
-        log.trace("attempt read from the channel (channel.ready:{}, key.isReadable:{}, " +
-            "channel.hasBytesBuffered:{}, hasStagedReceive: {}, explicitlyMutedChannels.contains(channel): {})",
+        log.trace("Attempt read from the channel (channel.ready:{}, key.isReadable:{}, " +
+            "channel.hasBytesBuffered:{}, hasStagedReceive:{}, explicitlyMutedChannels.contains(channel): {})",
             channel.ready(), key.isReadable(), channel.hasBytesBuffered(), hasStagedReceive(channel),
             explicitlyMutedChannels.contains(channel));
 

4df25a1c6c2436caea1d30928ad85ebe41491852
Lucas Wang <luwang@linkedin.com>
2022-08-31 14:10:11 +0000

    Fixing style

--- clients/src/main/java/org/apache/kafka/common/utils/PoisonPill.java
+++ clients/src/main/java/org/apache/kafka/common/utils/PoisonPill.java
@@ -24,10 +24,8 @@ import java.nio.file.StandardCopyOption;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.management.MBeanServer;
-import org.apache.kafka.common.MetricNameTemplate;
 import org.apache.kafka.common.metrics.Metrics;
 import org.apache.kafka.common.metrics.Sensor;
-import org.apache.kafka.common.metrics.stats.Avg;
 import org.apache.kafka.common.metrics.stats.Max;
 import org.apache.kafka.common.metrics.stats.Value;
 

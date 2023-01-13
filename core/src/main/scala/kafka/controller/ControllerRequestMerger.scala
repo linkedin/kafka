7eaa59d0eff8da052d849f5fee4bfd01d31d2cf8
Lucas Wang <luwang@linkedin.com>
2023-01-13 09:36:30 -0800

    Fix the exception message

--- core/src/main/scala/kafka/controller/ControllerRequestMerger.scala
+++ core/src/main/scala/kafka/controller/ControllerRequestMerger.scala
@@ -129,8 +129,8 @@ class ControllerRequestMerger(config: KafkaConfig) extends Logging {
     if (request.`type`().equals(LeaderAndIsrRequestType.FULL)) {
       // There should be no other LeaderAndISR partitions in the merger when a full LeaderAndISR request is sent
       if (!leaderAndIsrPartitionStates.isEmpty) {
-        throw new IllegalStateException("A Full LeaderAndISR request is received while some LeaderAndISR partitions are already " +
-        "ControllerRequest merger")
+        throw new IllegalStateException("A Full LeaderAndISR request is received " +
+          "while some LeaderAndISR partitions are already in the ControllerRequest merger")
       }
       isFullLeaderAndIsr = true
     }

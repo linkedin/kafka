dfb5ee0bcf9356acdb3fc0b965708bf0766cb0f6
Joseph (Ting-Chou) Lin <tlin@linkedin.com>
2023-10-05 22:51:29 -0700

    Add distribution

--- core/src/main/scala/kafka/server/ListOffsetsRequestInstrumentation.scala
+++ core/src/main/scala/kafka/server/ListOffsetsRequestInstrumentation.scala
@@ -1,6 +1,6 @@
 package kafka.server
 
-import com.yammer.metrics.core.Meter
+import com.yammer.metrics.core.{Histogram, Meter}
 import kafka.metrics.KafkaMetricsGroup
 import org.apache.kafka.common.message.ListOffsetsRequestData.ListOffsetsTopic
 import org.apache.kafka.common.requests.ListOffsetsRequest
@@ -16,16 +16,30 @@ import scala.jdk.CollectionConverters._
  * 2. what's the usage size of this
  */
 class ListOffsetsRequestInstrumentation extends KafkaMetricsGroup {
-  private val metricName = "ListOffsetsPartitionsRequested"
+  private val partitionRequestRate = "ListOffsetsPartitionsRequestRate"
+  private val partitionsPerRequest = "ListOffsetsPartitionsPerRequest"
   private val eventType = "partitions"
   private val listBy = "listBy"
+  private val UNKNOWN = "UNKNOWN"
+  private val EARLIEST = "EARLIEST"
+  private val LATEST = "LATEST"
+  private val LI_EARLIEST_LOCAL = "LI_EARLIEST_LOCAL"
+  private val MAX = "MAX"
+  private val BY_TIMESTAMP = "BY_TIMESTAMP"
 
-  private val unknownTimestamp: Meter = newMeter(metricName, eventType, TimeUnit.SECONDS, Map(listBy -> "UNKNOWN"))
-  private val earliestTimestamp: Meter = newMeter(metricName, eventType, TimeUnit.SECONDS, Map(listBy -> "EARLIEST"))
-  private val latestTimestamp: Meter = newMeter(metricName, eventType, TimeUnit.SECONDS, Map(listBy -> "LATEST"))
-  private val liEarliestLocalTimestamp: Meter = newMeter(metricName, eventType, TimeUnit.SECONDS, Map(listBy -> "LI_EARLIEST_LOCAL"))
-  private val maxTimestamp: Meter = newMeter(metricName, eventType, TimeUnit.SECONDS, Map(listBy -> "MAX"))
-  private val byTimestamp: Meter = newMeter(metricName, eventType, TimeUnit.SECONDS, Map(listBy -> "BY_TIMESTAMP"))
+  private val unknownTimestampMeter: Meter            = newMeter(partitionRequestRate, eventType, TimeUnit.SECONDS, Map(listBy -> UNKNOWN))
+  private val unknownTimestampHist: Histogram         = newHistogram(partitionsPerRequest, biased = true,           Map(listBy -> UNKNOWN))
+  private val earliestTimestampMeter: Meter           = newMeter(partitionRequestRate, eventType, TimeUnit.SECONDS, Map(listBy -> EARLIEST))
+  private val earliestTimestampHist: Histogram        = newHistogram(partitionsPerRequest, biased = true,           Map(listBy -> EARLIEST))
+  private val latestTimestampMeter: Meter             = newMeter(partitionRequestRate, eventType, TimeUnit.SECONDS, Map(listBy -> LATEST))
+  private val latestTimestampHist: Histogram          = newHistogram(partitionsPerRequest, biased = true,           Map(listBy -> LATEST))
+  private val liEarliestLocalTimestampMeter: Meter    = newMeter(partitionRequestRate, eventType, TimeUnit.SECONDS, Map(listBy -> LI_EARLIEST_LOCAL))
+  private val liEarliestLocalTimestampHist: Histogram = newHistogram(partitionsPerRequest, biased = true,           Map(listBy -> LI_EARLIEST_LOCAL))
+  private val maxTimestampMeter: Meter                = newMeter(partitionRequestRate, eventType, TimeUnit.SECONDS, Map(listBy -> MAX))
+  private val maxTimestampHist: Histogram             = newHistogram(partitionsPerRequest, biased = true,           Map(listBy -> MAX))
+  private val byTimestampMeter: Meter                 = newMeter(partitionRequestRate, eventType, TimeUnit.SECONDS, Map(listBy -> BY_TIMESTAMP))
+  private val byTimestampHist: Histogram              = newHistogram(partitionsPerRequest, biased = true,           Map(listBy -> BY_TIMESTAMP))
+
 
   // The object would be periodically dumped to log and cleared on kafka-server wrapper
   var listOffsetsByTimestampApiClientUsers: mutable.Map[String, mutable.Set[String]] = _
@@ -41,27 +55,51 @@ class ListOffsetsRequestInstrumentation extends KafkaMetricsGroup {
   }
 
   def logUsage(principal: KafkaPrincipal, topic: ListOffsetsTopic): Unit = {
+    var earliestCnt = 0;
+    var latestCnt = 0;
+    var liEarliestLocalCnt = 0;
+    var maxCnt = 0;
+    var unknownCnt = 0;
+    var byTimestampCnt = 0;
     topic.partitions().asScala.foreach { partition =>
       partition.timestamp() match {
         // special types like EARLIEST are constants < 0
-        case ListOffsetsRequest.EARLIEST_TIMESTAMP => earliestTimestamp.mark()
-        case ListOffsetsRequest.LATEST_TIMESTAMP => latestTimestamp.mark()
-        case ListOffsetsRequest.LI_EARLIEST_LOCAL_TIMESTAMP => liEarliestLocalTimestamp.mark()
-        case ListOffsetsRequest.MAX_TIMESTAMP => maxTimestamp.mark()
+        case ListOffsetsRequest.EARLIEST_TIMESTAMP => earliestCnt += 1
+        case ListOffsetsRequest.LATEST_TIMESTAMP => latestCnt += 1
+        case ListOffsetsRequest.LI_EARLIEST_LOCAL_TIMESTAMP => liEarliestLocalCnt += 1
+        case ListOffsetsRequest.MAX_TIMESTAMP => maxCnt += 1
         // Negative, not by actual timestamp, but also not yet defined constant type
-        case t if t < 0 => unknownTimestamp.mark()
+        case t if t < 0 => unknownCnt += 1
         // When > 0, it's specifying search by an actual timestamp
-        case t if t >= 0 =>
-          byTimestamp.mark()
-          // For by timestamp, we also want to know who are the ones sending
-          (listOffsetsByTimestampApiClientUsers.get(principal.getName) match {
-            case Some(v) => v
-            case None =>
-              val newSet: mutable.Set[String] = mutable.Set()
-              listOffsetsByTimestampApiClientUsers(principal.getName) = newSet
-              newSet
-          }).add(topic.name)
+        case t if t >= 0 => byTimestampCnt += 1
       }
+    }
+
+    // Mark partitions requested per second for each type
+    earliestTimestampMeter.mark(earliestCnt)
+    latestTimestampMeter.mark(latestCnt)
+    liEarliestLocalTimestampMeter.mark(liEarliestLocalCnt)
+    maxTimestampMeter.mark(maxCnt)
+    unknownTimestampMeter.mark(unknownCnt)
+    byTimestampMeter.mark(byTimestampCnt)
+
+    // Also mark the distribution of each type
+    earliestTimestampHist.update(earliestCnt)
+    latestTimestampHist.update(latestCnt)
+    liEarliestLocalTimestampHist.update(liEarliestLocalCnt)
+    maxTimestampHist.update(maxCnt)
+    unknownTimestampHist.update(unknownCnt)
+    byTimestampHist.update(byTimestampCnt)
+
+    if (byTimestampCnt > 0) {
+      // For by timestamp, we also want to know who are the ones sending
+      (listOffsetsByTimestampApiClientUsers.get(principal.getName) match {
+        case Some(v) => v
+        case None =>
+          val newSet: mutable.Set[String] = mutable.Set()
+          listOffsetsByTimestampApiClientUsers(principal.getName) = newSet
+          newSet
+      }).add(topic.name)
     }
   }
 }

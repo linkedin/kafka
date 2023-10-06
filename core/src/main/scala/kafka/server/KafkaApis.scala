7ad6bf4fb0f2003a9e506d9b29f77a93b3fdae12
Joseph (Ting-Chou) Lin <tlin@linkedin.com>
2023-10-06 14:53:56 -0700

    Add comment

--- core/src/main/scala/kafka/server/KafkaApis.scala
+++ core/src/main/scala/kafka/server/KafkaApis.scala
@@ -1128,6 +1128,11 @@ class KafkaApis(val requestChannel: RequestChannel,
 
     val responseTopics = authorizedRequestInfo.map { topic =>
       val isClientRequest = offsetRequest.replicaId == ListOffsetsRequest.CONSUMER_REPLICA_ID
+
+      // For the Northguard use case, we want to know what's the load on ListOffsets API used with "search by timestamp".
+      // - Partitions per second requested for listOffset by each type (earliest, by timestamp, latest, etc.)
+      // - Exposes the logging for "what are the principals querying such case"
+      // - Distribution of partitions included per request.
       if (isClientRequest) {  // Brokers send listOffset requests too to check if truncation needed --- ignore those.
         listOffsetsRequestInstrumentation.logUsage(request.context.principal, topic)
       }

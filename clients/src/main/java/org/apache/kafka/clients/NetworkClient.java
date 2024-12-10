f69849c32735a5dc7e7a2286d653c04eb246a65d
Yuhao Yang <philipyyh@gmail.com>
2024-12-10 10:37:52 -0800

    ACTIONITEM-3738: Improve the log message for the "There are no nodes in the Kafka cluster" error. (#523)
    * ACTIONITEM-3738: Improve the log message for the "There are no nodes in the Kafka cluster" error.
    
    * Improve error msg.
    
    * More improvements.
    
    ---------
    
    Co-authored-by: yuhayang@linkedin.com <alias@linkedin.com>

--- clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
+++ clients/src/main/java/org/apache/kafka/clients/NetworkClient.java
@@ -888,7 +888,8 @@ public class NetworkClient implements KafkaClient {
     public Node leastLoadedNode(long now) {
         List<Node> nodes = this.metadataUpdater.fetchNodes();
         if (nodes.isEmpty())
-            throw new IllegalStateException("There are no nodes in the Kafka cluster");
+            throw new IllegalStateException("metadataUpdater returned empty node list. "
+                + "The client is not able to connect to the Kafka cluster or there are no nodes in the Kafka cluster.");
         LeastLoadedNodeAlgorithm algo = this.leastLoadedNodeAlgorithm;
         if (algo == null) {
             throw new IllegalStateException("leastLoadedNodeAlgorithm cannot be null");

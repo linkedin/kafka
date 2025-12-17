dcc31b0bdcb6049644706a7e67013457914387b1
Jon Bringhurst <jon@bringhurst.org>
2025-12-17 11:37:32 -0800

    Wire in truncation listener to Log

--- core/src/main/scala/kafka/log/Log.scala
+++ core/src/main/scala/kafka/log/Log.scala
@@ -28,6 +28,7 @@ import kafka.api.{ApiVersion, KAFKA_0_10_0_IV0}
 import kafka.common.{LongRef, OffsetsOutOfOrderException, UnexpectedAppendOffsetException}
 import kafka.log.AppendOrigin.RaftLeader
 import kafka.log.remote.RemoteLogManager
+import kafka.log.TruncationCallbacks
 import kafka.message.{BrokerCompressionCodec, CompressionCodec, NoCompressionCodec}
 import kafka.metrics.KafkaMetricsGroup
 import kafka.server.checkpoints.LeaderEpochCheckpointFile
@@ -2021,7 +2022,6 @@ class Log(@volatile private var _dir: File,
         lock synchronized {
           leaderEpochCache.foreach(_.truncateFromEnd(logEndOffset))
         }
-
         false
       } else {
         var bytesTruncated = 0L
@@ -2054,6 +2054,18 @@ class Log(@volatile private var _dir: File,
               endOffset = targetOffset
             )
           }
+
+          // Notify log truncation listeners
+          TruncationCallbacks.notify(
+            topicPartition,
+            op      = "truncateTo",
+            target  = targetOffset,
+            fromLeo = originalLogEndOffset,
+            toLeo   = offsetTruncatedTo,
+            msgs    = messagesTruncated,
+            bytes   = bytesTruncated
+          )
+
           // FIXME: this code path involves not only data plane segments but also KRaft metadata logs.  Should find a way to distinguish after moving to KRaft.
           // XXX: An internal dashboard depends on parsing this warn log line. Get SRE reviews before changing the format.
           warn(s"Attempted truncating to offset $targetOffset. Resulted in truncated to $offsetTruncatedTo from the original log end offset $originalLogEndOffset, " +
@@ -2076,7 +2088,10 @@ class Log(@volatile private var _dir: File,
       debug(s"Truncate and start at offset $newOffset")
       lock synchronized {
         checkIfMemoryMappedBufferClosed()
-        removeAndDeleteSegments(logSegments, asyncDelete = true, LogTruncation)
+
+        val originalLogEndOffset = logEndOffset
+        val bytesTruncated = removeAndDeleteSegments(logSegments, asyncDelete = true, LogTruncation)
+
         addSegment(LogSegment.open(dir,
           baseOffset = newOffset,
           config = config,
@@ -2090,6 +2105,18 @@ class Log(@volatile private var _dir: File,
           startOffset = newOffset,
           localLogStartOffset = newOffset,
           endOffset = newOffset
+        )
+
+        // Notify log truncation listeners
+        val messagesTruncated = math.max(0L, originalLogEndOffset - newOffset)
+        TruncationCallbacks.notify(
+          topicPartition,
+          op      = "truncateFullyAndStartAt",
+          target  = newOffset,
+          fromLeo = originalLogEndOffset,
+          toLeo   = newOffset,
+          msgs    = messagesTruncated,
+          bytes   = bytesTruncated
         )
       }
     }

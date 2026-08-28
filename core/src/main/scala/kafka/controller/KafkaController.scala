linkedin/kafka

commit b5fa0af654674b357f09305556d9c5d223c2f9ce

[LI-HOTFIX] Add topic deletion hook in zookeeper to trigger in-memory topic deletion flag changes
TICKET =
LI_DESCRIPTION =

- Add TopicDeletionFlagPath `/topic_deletion_flag` to trigger changes in topic deletion enable flag in TopicDeletionManager.
- Controller will listen on this path when active and un-listen when resignation.
- If the znode is deleted, the topic deletion flag will be set according to the config.

RB=1306354
BUG=LIKAFKA-17224
G=Kafka-Code-Reviews
R=luwang
A=luwang

EXIT_CRITERIA = MANUAL ["After upstream makes delete.topic.enable a dynamic broker config"]

core/src/main/scala/kafka/controller/KafkaController.scala
@@ -118,6 +118,7 @@ class KafkaController(val config: KafkaConfig,
   private val preferredReplicaElectionHandler = new PreferredReplicaElectionHandler(eventManager)
   private val isrChangeNotificationHandler = new IsrChangeNotificationHandler(eventManager)
   private val logDirEventNotificationHandler = new LogDirEventNotificationHandler(eventManager)
+  private val topicDeletionFlagHandler = new TopicDeletionFlagHandler(this, eventManager)
 
   @volatile private var activeControllerId = -1
   @volatile private var offlinePartitionCount = 0
@@ -237,7 +238,7 @@ class KafkaController(val config: KafkaConfig,
       isrChangeNotificationHandler)
     childChangeHandlers.foreach(zkClient.registerZNodeChildChangeHandler)
 
-    val nodeChangeHandlers = Seq(preferredReplicaElectionHandler, partitionReassignmentHandler)
+    val nodeChangeHandlers = Seq(preferredReplicaElectionHandler, partitionReassignmentHandler, topicDeletionFlagHandler)
     nodeChangeHandlers.foreach(zkClient.registerZNodeChangeHandlerAndCheckExistence)
 
     info("Deleting log dir event notifications")
@@ -478,6 +479,7 @@ class KafkaController(val config: KafkaConfig,
     zkClient.unregisterZNodeChildChangeHandler(topicChangeHandler.path)
     unregisterPartitionModificationsHandlers(partitionModificationsHandlers.keys.toSeq)
     zkClient.unregisterZNodeChildChangeHandler(topicDeletionHandler.path)
+    zkClient.unregisterZNodeChangeHandler(topicDeletionFlagHandler.path)
     // shutdown replica state machine
     replicaStateMachine.shutdown()
     zkClient.unregisterZNodeChildChangeHandler(brokerChangeHandler.path)
@@ -1744,7 +1746,7 @@ class KafkaController(val config: KafkaConfig,
       zkClient.deleteTopicDeletions(nonExistentTopics.toSeq, controllerContext.epochZkVersion)
     }
     topicsToBeDeleted --= nonExistentTopics
-    if (config.deleteTopicEnable) {
+    if (topicDeletionManager.isDeleteTopicEnabled) {
       if (topicsToBeDeleted.nonEmpty) {
         info(s"Starting topic deletion for topics ${topicsToBeDeleted.mkString(",")}")
         // mark topic ineligible for deletion if other state changes are in progress
@@ -1762,6 +1764,24 @@ class KafkaController(val config: KafkaConfig,
       // If delete topic is disabled remove entries under zookeeper path : /admin/delete_topics
       info(s"Removing $topicsToBeDeleted since delete topic is disabled")
       zkClient.deleteTopicDeletions(topicsToBeDeleted.toSeq, controllerContext.epochZkVersion)
+    }
+  }
+
+  private def processTopicDeletionFlagChange(reset: Boolean): Unit = {
+    info("Process TopicDeletionFlagChange event")
+    if (!isActive) return
+    if (reset)
+      topicDeletionManager.resetDeleteTopicEnabled()
+    else {
+      val topicDeletionFlag = zkClient.getTopicDeletionFlag
+      if (!topicDeletionFlag.equalsIgnoreCase("true") && !topicDeletionFlag.equalsIgnoreCase("false")) {
+        info(s"Overwrite ${DeleteTopicFlagZNode.path} to ${topicDeletionManager.isDeleteTopicEnabled}")
+        zkClient.setTopicDeletionFlag(topicDeletionManager.isDeleteTopicEnabled.toString)
+      }
+      else {
+        info(s"Set isDeleteTopicEnabled flag to $topicDeletionFlag")
+        topicDeletionManager.isDeleteTopicEnabled = topicDeletionFlag.toBoolean
+      }
     }
   }
 
@@ -2508,6 +2528,8 @@ class KafkaController(val config: KafkaConfig,
           processListPartitionReassignments(partitions, callback)
         case UpdateFeatures(request, callback) =>
           processFeatureUpdates(request, callback)
+        case TopicDeletionFlagChange(reset) =>
+          processTopicDeletionFlagChange(reset)
         case PartitionReassignmentIsrChange(partition) =>
           processPartitionReassignmentIsrChange(partition)
         case IsrChangeNotification =>
@@ -2577,6 +2599,19 @@ class TopicDeletionHandler(eventManager: ControllerEventManager) extends ZNodeCh
   override val path: String = DeleteTopicsZNode.path
 
   override def handleChildChange(): Unit = eventManager.put(TopicDeletion)
+}
+/**
+  * Listener for /topic_deletion_flag znode.
+  *   If the data of the znode is set to true/false, it will trigger the in memory isDeleteTopicEnabled to be set accordingly.
+  *   If the znode data cannot be converted to boolean, it will overwrite znode with the previous valid value.
+  *   If the znode path is deleted, it will reset the in memory isDeleteTopicEnabled to the config value.
+  */
+class TopicDeletionFlagHandler(controller: KafkaController, eventManager: ControllerEventManager) extends ZNodeChangeHandler {
+  override val path: String = DeleteTopicFlagZNode.path
+
+  override def handleDataChange(): Unit = eventManager.put(TopicDeletionFlagChange())
+
+  override def handleDeletion(): Unit = eventManager.put(TopicDeletionFlagChange(true))
 }
 
 class PartitionReassignmentHandler(eventManager: ControllerEventManager) extends ZNodeChangeHandler {
@@ -2776,6 +2811,11 @@ case object IsrChangeNotification extends ControllerEvent {
 case class AlterIsrReceived(brokerId: Int, brokerEpoch: Long, isrsToAlter: Map[TopicPartition, LeaderAndIsr],
                             callback: AlterIsrCallback) extends ControllerEvent {
   override def state: ControllerState = ControllerState.IsrChange
+  override def preempt(): Unit = {}
+}
+
+case class TopicDeletionFlagChange(reset: Boolean = false) extends ControllerEvent {
+  def state = ControllerState.TopicDeletionFlagChange
   override def preempt(): Unit = {}
 }
 

core/src/main/scala/kafka/controller/TopicDeletionManager.scala
@@ -20,6 +20,7 @@ import kafka.server.KafkaConfig
 import kafka.utils.Logging
 import kafka.zk.KafkaZkClient
 import org.apache.kafka.common.TopicPartition
+import org.apache.zookeeper.KeeperException.{NoNodeException, NodeExistsException}
 
 import scala.collection.Set
 import scala.collection.mutable
@@ -29,6 +30,8 @@ trait DeletionClient {
   def deleteTopicDeletions(topics: Seq[String], epochZkVersion: Int): Unit
   def mutePartitionModifications(topic: String): Unit
   def sendMetadataUpdate(partitions: Set[TopicPartition]): Unit
+  def createDeleteTopicFlagPath(): Unit
+  def getTopicDeletionFlag(): String
 }
 
 class ControllerDeletionClient(controller: KafkaController, zkClient: KafkaZkClient) extends DeletionClient {
@@ -48,6 +51,14 @@ class ControllerDeletionClient(controller: KafkaController, zkClient: KafkaZkCli
 
   override def sendMetadataUpdate(partitions: Set[TopicPartition]): Unit = {
     controller.sendUpdateMetadataRequest(controller.controllerContext.liveOrShuttingDownBrokerIds.toSeq, partitions)
+  }
+
+  override def createDeleteTopicFlagPath(): Unit = {
+    zkClient.createDeleteTopicFlagPath
+  }
+
+  override def getTopicDeletionFlag(): String = {
+    zkClient.getTopicDeletionFlag
   }
 }
 
@@ -90,11 +101,20 @@ class TopicDeletionManager(config: KafkaConfig,
                            partitionStateMachine: PartitionStateMachine,
                            client: DeletionClient) extends Logging {
   this.logIdent = s"[Topic Deletion Manager ${config.brokerId}] "
-  val isDeleteTopicEnabled: Boolean = config.deleteTopicEnable
+  var isDeleteTopicEnabled: Boolean = config.deleteTopicEnable
+
+  // Try to create the znode for delete topic flag
+  try {
+    client.createDeleteTopicFlagPath()
+  } catch {
+    case _: NodeExistsException =>
+  }
 
   def init(initialTopicsToBeDeleted: Set[String], initialTopicsIneligibleForDeletion: Set[String]): Unit = {
     info(s"Initializing manager with initial deletions: $initialTopicsToBeDeleted, " +
       s"initial ineligible deletions: $initialTopicsIneligibleForDeletion")
+
+    isDeleteTopicEnabled = getDeleteTopicEnabled()
 
     if (isDeleteTopicEnabled) {
       controllerContext.queueTopicDeletion(initialTopicsToBeDeleted)
@@ -354,5 +374,21 @@ class TopicDeletionManager(config: KafkaConfig,
     if (topicsEligibleForDeletion.nonEmpty) {
       onTopicDeletion(topicsEligibleForDeletion)
     }
+  }
+
+  private def getDeleteTopicEnabled(): Boolean = {
+    try {
+      val deleteTopicFlag = client.getTopicDeletionFlag
+      if (deleteTopicFlag == null || (!deleteTopicFlag.equalsIgnoreCase("true") && !deleteTopicFlag.equalsIgnoreCase("false")))
+        isDeleteTopicEnabled
+      else deleteTopicFlag.toBoolean
+    } catch {
+      case _: NoNodeException => config.deleteTopicEnable
+    }
+  }
+
+  def resetDeleteTopicEnabled(): Unit = {
+    info("Reset isDeleteTopicEnabled flag to %s".format(config.deleteTopicEnable))
+    isDeleteTopicEnabled = config.deleteTopicEnable
   }
 }

core/src/main/scala/kafka/zk/KafkaZkClient.scala
@@ -865,6 +865,37 @@ class KafkaZkClient private[zk] (zooKeeperClient: ZooKeeperClient, isSecure: Boo
   }
 
   /**
+   * Creates the delete topic flag znode.
+   * @throws KeeperException if there is an error while setting or creating the znode
+   */
+  def createDeleteTopicFlagPath(): Unit = {
+    createRecursive(DeleteTopicFlagZNode.path)
+  }
+
+  /**
+   * Get topic deletion flag in zookeeper.
+   * @return topic deletion flag in zookeeper.
+   */
+  def getTopicDeletionFlag: String = {
+    val getDataResponse = retryRequestUntilConnected(GetDataRequest(DeleteTopicFlagZNode.path))
+    getDataResponse.resultCode match {
+      case Code.OK => DeleteTopicFlagZNode.decode(getDataResponse.data)
+      case _ => throw getDataResponse.resultException.get
+    }
+  }
+
+  /**
+   * Set topic deletion flag in zookeeper.
+   */
+  def setTopicDeletionFlag(flag: String): Unit = {
+    val setDataResponse = retryRequestUntilConnected(SetDataRequest(DeleteTopicFlagZNode.path, DeleteTopicFlagZNode.encode(flag), -1))
+    setDataResponse.resultCode match {
+      case Code.OK =>
+      case _ => throw setDataResponse.resultException.get
+    }
+  }
+
+  /**
    * Remove the given topics from the topics marked for deletion.
    * @param topics the topics to remove.
    * @param expectedControllerEpochZkVersion expected controller epoch zkVersion.

core/src/main/scala/kafka/zk/ZkData.scala
@@ -455,6 +455,12 @@ object DeleteTopicsTopicZNode {
   def path(topic: String) = s"${DeleteTopicsZNode.path}/$topic"
 }
 
+object DeleteTopicFlagZNode {
+  def path = "/topic_deletion_flag"
+  def encode(topicDeletionFlag: String): Array[Byte] = topicDeletionFlag.getBytes(UTF_8)
+  def decode(bytes: Array[Byte]): String = if (bytes != null) new String(bytes, UTF_8) else ""
+}
+
 /**
  * The znode for initiating a partition reassignment.
  * @deprecated Since 2.4, use the PartitionReassignment Kafka API instead.

core/src/main/scala/kafka/controller/ControllerState.scala
@@ -114,9 +114,13 @@ object ControllerState {
     def value = 17
   }
 
+  case object TopicDeletionFlagChange extends ControllerState {
+    def value = 100
+  }
+
   val values: Seq[ControllerState] = Seq(Idle, ControllerChange, BrokerChange, TopicChange, TopicDeletion,
     AlterPartitionReassignment, AutoLeaderBalance, ManualLeaderBalance, ControlledShutdown, IsrChange,
     LeaderAndIsrResponseReceived, LogDirChange, ControllerShutdown, UncleanLeaderElectionEnable,
     TopicUncleanLeaderElectionEnable, ListPartitionReassignment, UpdateMetadataResponseReceived,
-    UpdateFeatures)
+    UpdateFeatures, TopicDeletionFlagChange)
 }

core/src/test/scala/unit/kafka/admin/DeleteTopicTest.scala
@@ -22,7 +22,7 @@ import java.util.{Collections, Optional, Properties}
 
 import scala.collection.Seq
 import kafka.log.Log
-import kafka.zk.{TopicPartitionZNode, ZooKeeperTestHarness}
+import kafka.zk.{DeleteTopicFlagZNode, TopicPartitionZNode, TopicZNode, ZooKeeperTestHarness}
 import kafka.utils.TestUtils
 import kafka.server.{KafkaConfig, KafkaServer}
 import org.junit.jupiter.api.Assertions._
@@ -415,6 +415,62 @@ class DeleteTopicTest extends ZooKeeperTestHarness {
     // topic test should have a leader
     val leaderIdOpt = zkClient.getLeaderForPartition(new TopicPartition(topic, 0))
     assertTrue(leaderIdOpt.isDefined, "Leader should exist for topic test")
+  }
+
+  def testDeleteTopicAfterEnableZkDeleteTopicFlag() {
+    val topicPartition = new TopicPartition("test", 0)
+    val topic = topicPartition.topic
+    servers = createTestTopicAndCluster(topic, deleteTopicEnabled = false)
+    // mark the topic for deletion
+    adminZkClient.deleteTopic("test")
+    TestUtils.waitUntilTrue(() => !zkClient.isTopicMarkedForDeletion(topic),
+      "Admin path /admin/delete_topic/%s path not deleted even if deleteTopic is disabled".format(topic))
+    // verify that topic test is untouched
+    assertTrue(servers.forall(_.getLogManager.getLog(topicPartition).isDefined))
+    // test the topic path exists
+    assertTrue(zkClient.pathExists(TopicZNode.path(topic)), "Topic path disappeared even when topic deletion is disabled")
+    // topic test should have a leader
+    val leaderIdOpt = zkClient.getLeaderForPartition(new TopicPartition(topic, 0))
+    assertTrue(leaderIdOpt.isDefined, "Leader should exist for topic test")
+
+    // Set TopicDeletionFlag to true in zk and try delete topic again
+    zkClient.setTopicDeletionFlag("true")
+    TestUtils.waitUntilTrue(() =>
+      try {
+        zkClient.getTopicDeletionFlag.equalsIgnoreCase("true")
+      } catch {
+        case _: Throwable => false
+      },
+      "TopicDeletionFlag is not set")
+    TestUtils.waitUntilTrue( () => getController()._1.kafkaController.topicDeletionManager.isDeleteTopicEnabled,
+      "Delete topic is not enabled")
+    // mark the topic for deletion
+    adminZkClient.deleteTopic("test")
+    TestUtils.verifyTopicDeletion(zkClient, "test", 1, servers)
+
+    // Set TopicDeletionFlag to invalid value in zk
+    zkClient.setTopicDeletionFlag("flase")
+    TestUtils.waitUntilTrue(() =>
+      try {
+        zkClient.getTopicDeletionFlag.equalsIgnoreCase("true")
+      } catch {
+        case _: Throwable => false
+      },
+      "TopicDeletionFlag is not overwritten")
+
+    // delete TopicDeletionFlagPath in zk
+    zkClient.deletePath(DeleteTopicFlagZNode.path)
+    TestUtils.waitUntilTrue(() =>
+      try {
+        !zkClient.pathExists(DeleteTopicFlagZNode.path)
+      } catch {
+        case _: Throwable => false
+      },
+      "TopicDeletionFlagPath is not deleted")
+    TestUtils.waitUntilTrue(() =>
+      getController()._1.kafkaController.topicDeletionManager.isDeleteTopicEnabled == false,
+      "Topic deletion flag is not rest"
+    )
   }
 
   @Test

commit 9576cfa5c8fb8384730e06924ea377ef3e4e5cbb

kafka: restore dynamic ZooKeeper topic deletion control
Production starts with delete.topic.enable disabled and changes the live
controller behavior through /topic_deletion_flag. Restore that watched
ZooKeeper setting so deletion can be enabled or disabled without a broker
restart.

Gate the behavior behind a disabled-by-default static compatibility flag.
When enabled, initialize the znode from the broker config if it has no
valid value, update TopicDeletionManager on data changes, and return to
the static config when the znode is deleted. Expose the feature state in
the per-broker compatibility metrics.

The 3.0-li feature and its broker-request fix were introduced in:
https://github.com/linkedin/kafka/commit/b5fa0af654
https://github.com/linkedin/kafka/commit/06152cad19

core/src/main/scala/kafka/controller/KafkaController.scala
@@ -168,6 +168,7 @@ class KafkaController(val config: KafkaConfig,
   private val preferredReplicaElectionHandler = new PreferredReplicaElectionHandler(eventManager)
   private val isrChangeNotificationHandler = new IsrChangeNotificationHandler(eventManager)
   private val logDirEventNotificationHandler = new LogDirEventNotificationHandler(eventManager)
+  private val topicDeletionFlagHandler = new TopicDeletionFlagHandler(eventManager)
 
   @volatile var activeControllerId = -1
   @volatile private var offlinePartitionCount = 0
@@ -313,6 +314,8 @@ class KafkaController(val config: KafkaConfig,
 
     val nodeChangeHandlers = Seq(preferredReplicaElectionHandler, partitionReassignmentHandler)
     nodeChangeHandlers.foreach(zkClient.registerZNodeChangeHandlerAndCheckExistence)
+    if (config.liProtocolBridgeDynamicTopicDeletionActive)
+      zkClient.registerZNodeChangeHandlerAndCheckExistence(topicDeletionFlagHandler)
 
     info("Deleting log dir event notifications")
     zkClient.deleteLogDirEventNotifications(controllerContext.epochZkVersion)
@@ -320,6 +323,8 @@ class KafkaController(val config: KafkaConfig,
     zkClient.deleteIsrChangeNotifications(controllerContext.epochZkVersion)
     info("Initializing controller context")
     initializeControllerContext()
+    if (config.liProtocolBridgeDynamicTopicDeletionActive)
+      processTopicDeletionFlagChange(reset = false)
     info("Fetching topic deletions in progress")
     val (topicsToBeDeleted, topicsIneligibleForDeletion) = fetchTopicDeletionsInProgress()
     info("Initializing topic deletion manager")
@@ -1910,6 +1915,21 @@ class KafkaController(val config: KafkaConfig,
     }
   }
 
+  private def processTopicDeletionFlagChange(reset: Boolean): Unit = {
+    if (!isActive || !config.liProtocolBridgeDynamicTopicDeletionActive)
+      return
+    if (reset) {
+      topicDeletionManager.resetDeleteTopicEnabled()
+    } else {
+      zkClient.getTopicDeletionFlag match {
+        case Some(enabled) => topicDeletionManager.isDeleteTopicEnabled = enabled
+        case None =>
+          topicDeletionManager.resetDeleteTopicEnabled()
+          zkClient.setTopicDeletionFlag(topicDeletionManager.isDeleteTopicEnabled)
+      }
+    }
+  }
+
   private def processZkPartitionReassignment(): Set[TopicPartition] = {
     // We need to register the watcher if the path doesn't exist in order to detect future
     // reassignments and we get the `path exists` check for free
@@ -2756,6 +2776,8 @@ class KafkaController(val config: KafkaConfig,
           processPartitionModifications(topic)
         case TopicDeletion =>
           processTopicDeletion()
+        case TopicDeletionFlagChange(reset) =>
+          processTopicDeletionFlagChange(reset)
         case ApiPartitionReassignment(reassignments, callback) =>
           processApiPartitionReassignment(reassignments, callback)
         case ZkPartitionReassignment =>
@@ -2838,6 +2860,12 @@ class TopicDeletionHandler(eventManager: ControllerEventManager) extends ZNodeCh
   override val path: String = DeleteTopicsZNode.path
 
   override def handleChildChange(): Unit = eventManager.put(TopicDeletion)
+}
+
+class TopicDeletionFlagHandler(eventManager: ControllerEventManager) extends ZNodeChangeHandler {
+  override val path: String = DeleteTopicFlagZNode.path
+  override def handleDataChange(): Unit = eventManager.put(TopicDeletionFlagChange(reset = false))
+  override def handleDeletion(): Unit = eventManager.put(TopicDeletionFlagChange(reset = true))
 }
 
 class PartitionReassignmentHandler(eventManager: ControllerEventManager) extends ZNodeChangeHandler {
@@ -3029,6 +3057,11 @@ case class PartitionModifications(topic: String) extends ControllerEvent {
 }
 
 case object TopicDeletion extends ControllerEvent {
+  override def state: ControllerState = ControllerState.TopicDeletion
+  override def preempt(): Unit = {}
+}
+
+case class TopicDeletionFlagChange(reset: Boolean) extends ControllerEvent {
   override def state: ControllerState = ControllerState.TopicDeletion
   override def preempt(): Unit = {}
 }

core/src/main/scala/kafka/controller/TopicDeletionManager.scala
@@ -89,7 +89,11 @@ class TopicDeletionManager(config: KafkaConfig,
                            partitionStateMachine: PartitionStateMachine,
                            client: DeletionClient) extends Logging {
   this.logIdent = s"[Topic Deletion Manager ${config.brokerId}] "
-  val isDeleteTopicEnabled: Boolean = config.deleteTopicEnable
+  @volatile var isDeleteTopicEnabled: Boolean = config.deleteTopicEnable
+
+  def resetDeleteTopicEnabled(): Unit = {
+    isDeleteTopicEnabled = config.deleteTopicEnable
+  }
 
   def init(initialTopicsToBeDeleted: Set[String], initialTopicsIneligibleForDeletion: Set[String]): Unit = {
     info(s"Initializing manager with initial deletions: $initialTopicsToBeDeleted, " +

core/src/main/scala/kafka/server/KafkaConfig.scala
@@ -73,6 +73,8 @@ object KafkaConfig {
     "li.protocol.bridge.federated.topics.enable"
   val LiProtocolBridgeRackIdMapperEnableProp =
     "li.protocol.bridge.rack.id.mapper.enable"
+  val LiProtocolBridgeDynamicTopicDeletionEnableProp =
+    "li.protocol.bridge.dynamic.topic.deletion.enable"
 
   // 3.0-li operational settings consumed by the LinkedIn server wrapper.
   val PreferredControllerProp = "preferred.controller"
@@ -146,6 +148,8 @@ object KafkaConfig {
     "Enable the 3.0-li federated-topic APIs and ZooKeeper metadata used by the LinkedIn authorizer."
   val LiProtocolBridgeRackIdMapperEnableDoc =
     "Apply the configured 3.0-li rack ID mapper during automatic replica assignment."
+  val LiProtocolBridgeDynamicTopicDeletionEnableDoc =
+    "Read delete.topic.enable dynamically from the 3.0-li /topic_deletion_flag ZooKeeper path."
   val PreferredControllerDoc = "Whether this broker is eligible for preferred-controller placement."
   val AllowPreferredControllerFallbackDoc =
     "Allow a non-preferred broker to become controller when no preferred controller is available."
@@ -207,6 +211,8 @@ object KafkaConfig {
       ConfigDef.Importance.HIGH, LiProtocolBridgeFederatedTopicsEnableDoc)
     .define(LiProtocolBridgeRackIdMapperEnableProp, ConfigDef.Type.BOOLEAN, false,
       ConfigDef.Importance.HIGH, LiProtocolBridgeRackIdMapperEnableDoc)
+    .define(LiProtocolBridgeDynamicTopicDeletionEnableProp, ConfigDef.Type.BOOLEAN, false,
+      ConfigDef.Importance.HIGH, LiProtocolBridgeDynamicTopicDeletionEnableDoc)
     .define(PreferredControllerProp, ConfigDef.Type.BOOLEAN, false,
       ConfigDef.Importance.HIGH, PreferredControllerDoc)
     .define(AllowPreferredControllerFallbackProp, ConfigDef.Type.BOOLEAN, true,
@@ -342,6 +348,8 @@ class KafkaConfig private(doLog: Boolean, val props: util.Map[_, _])
     getBoolean(KafkaConfig.LiProtocolBridgeFederatedTopicsEnableProp)
   def liProtocolBridgeRackIdMapperEnable: Boolean =
     getBoolean(KafkaConfig.LiProtocolBridgeRackIdMapperEnableProp)
+  def liProtocolBridgeDynamicTopicDeletionEnable: Boolean =
+    getBoolean(KafkaConfig.LiProtocolBridgeDynamicTopicDeletionEnableProp)
 
   def liProtocolBridgeModeActive: Boolean = processRoles.isEmpty && liProtocolBridgeModeEnable
   def liProtocolBridgeFollowerRecoveryActive: Boolean =
@@ -358,6 +366,8 @@ class KafkaConfig private(doLog: Boolean, val props: util.Map[_, _])
     processRoles.isEmpty && liProtocolBridgeFederatedTopicsEnable
   def liProtocolBridgeRackIdMapperActive: Boolean =
     processRoles.isEmpty && liProtocolBridgeRackIdMapperEnable
+  def liProtocolBridgeDynamicTopicDeletionActive: Boolean =
+    processRoles.isEmpty && liProtocolBridgeDynamicTopicDeletionEnable
 
   lazy val rackIdMapperForReplicaAssignment: RackAwareReplicaAssignmentRackIdMapper = {
     val className = getString(KafkaConfig.LiRackIdMapperClassNameForRackAwareReplicaAssignmentProp)

core/src/main/scala/kafka/server/LiProtocolBridgeMetrics.scala
@@ -31,10 +31,11 @@ object LiProtocolBridgeMetrics {
   val FederatedTopicsEnabled = "FederatedTopicsEnabled"
   val RackIdMapperEnabled = "RackIdMapperEnabled"
   val ZookeeperPaginationEnabled = "ZookeeperPaginationEnabled"
+  val DynamicTopicDeletionEnabled = "DynamicTopicDeletionEnabled"
   val MetricNames: Seq[String] = Seq(ModeEnabled, FollowerRecoveryEnabled,
     RecommendedLeaderElectionEnabled, ExcludePartitionsEnabled, MoveControllerEnabled,
     ShutdownSafetyOverrideEnabled, PreferredControllerEnabled, FederatedTopicsEnabled,
-    RackIdMapperEnabled, ZookeeperPaginationEnabled)
+    RackIdMapperEnabled, ZookeeperPaginationEnabled, DynamicTopicDeletionEnabled)
 }
 
 /** Exposes the effective value of every 3.0-li compatibility flag on each ZooKeeper broker. */
@@ -63,6 +64,8 @@ class LiProtocolBridgeMetrics(config: KafkaConfig) extends AutoCloseable {
     () => enabled(config.liProtocolBridgeRackIdMapperActive), tags)
   metricsGroup.newGauge(ZookeeperPaginationEnabled,
     () => enabled(config.liZookeeperPaginationEnable), tags)
+  metricsGroup.newGauge(DynamicTopicDeletionEnabled,
+    () => enabled(config.liProtocolBridgeDynamicTopicDeletionActive), tags)
 
   private def enabled(value: Boolean): Int = if (value) 1 else 0
 

core/src/main/scala/kafka/zk/KafkaZkClient.scala
@@ -1337,6 +1337,21 @@ class KafkaZkClient private[zk] (
     retryRequestUntilConnected(deleteRequest, expectedControllerEpochZkVersion)
   }
 
+  def getTopicDeletionFlag: Option[Boolean] = {
+    val response = retryRequestUntilConnected(GetDataRequest(DeleteTopicFlagZNode.path))
+    response.resultCode match {
+      case Code.OK => DeleteTopicFlagZNode.decode(response.data)
+      case Code.NONODE => None
+      case _ => throw response.resultException.get
+    }
+  }
+
+  def setTopicDeletionFlag(enabled: Boolean): Unit = {
+    val response = retryRequestUntilConnected(
+      SetDataRequest(DeleteTopicFlagZNode.path, DeleteTopicFlagZNode.encode(enabled), ZkVersion.MatchAnyVersion))
+    response.maybeThrow()
+  }
+
   def createFederatedTopicZNode(topic: String, namespace: String): Unit =
     createRecursive(FederatedTopicZNode.path(topic, namespace))
 

core/src/main/scala/kafka/zk/ZkData.scala
@@ -92,6 +92,16 @@ object ConfigZNode {
   def path = "/config"
 }
 
+object DeleteTopicFlagZNode {
+  def path = "/topic_deletion_flag"
+  def encode(value: Boolean): Array[Byte] = value.toString.getBytes(UTF_8)
+  def decode(bytes: Array[Byte]): Option[Boolean] =
+    Option(bytes).map(new String(_, UTF_8).trim.toLowerCase).collect {
+      case "true" => true
+      case "false" => false
+    }
+}
+
 object BrokersZNode {
   def path = "/brokers"
 }
@@ -1123,6 +1133,7 @@ object ZkData {
   // These are persistent ZK paths that should exist on kafka broker startup.
   val PersistentZkPaths: Seq[String] = Seq(
     ConsumerPathZNode.path, // old consumer path
+    DeleteTopicFlagZNode.path,
     BrokerIdsZNode.path,
     FederatedTopicsZNode.path,
     PreferredControllersZNode.path,

core/src/test/scala/unit/kafka/server/LiProtocolBridgeMetricsTest.scala
@@ -51,7 +51,8 @@ class LiProtocolBridgeMetricsTest {
       val updatedValues = metricValues(brokerId)
       assertEquals(LiProtocolBridgeMetrics.MetricNames.toSet, updatedValues.keySet)
       val staticMetrics = Set(LiProtocolBridgeMetrics.PreferredControllerEnabled,
-        LiProtocolBridgeMetrics.RackIdMapperEnabled, LiProtocolBridgeMetrics.ZookeeperPaginationEnabled)
+        LiProtocolBridgeMetrics.RackIdMapperEnabled, LiProtocolBridgeMetrics.ZookeeperPaginationEnabled,
+        LiProtocolBridgeMetrics.DynamicTopicDeletionEnabled)
       staticMetrics.foreach(name => assertEquals(0, updatedValues(name)))
       assertTrue(updatedValues.filterNot { case (name, _) => staticMetrics.contains(name) }.values.forall(_ == 1))
     } finally {